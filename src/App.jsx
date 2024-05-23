b9384af88be643b96e23830788938d5ad4e2afe8
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-05-23 22:32:00 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+function Header() {
+  return (
+    <>
+      <div className="title">
+        <h1>Aaryan Mishra</h1>
+      </div>
+    </>
+  )
+}
+
 export default function App() {
   return (
     <>
-      <p>hello, world</p>
+      <Header />
     </>
   )
 }
