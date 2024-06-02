fa2acca0c899b136b44dc83a897d3c2e98052e0e
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-06-03 01:03:27 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-function Header() {
-  return (
-    <>
-      <div className="title">
-        <h1>Aaryan Mishra</h1>
-      </div>
-    </>
-  )
-}
+import './App.css'
 
 export default function App() {
   return (
     <>
-      <Header />
+      <h1>Aaryan Mishra</h1>
+
+      <hr></hr>
+
+      <nav>
+        <a href='https://bearblog.dev/' target='_blank'>
+          <li>Blog</li>
+        </a>
+
+        <a href='' target='_blank'>
+          <li>Projects</li>
+        </a>
+
+        <a href='https://github.com/mshra' target='_blank'>
+          <li>GitHub</li>
+        </a>
+
+        <a href='' target='_blank'>
+          <li>About</li>
+        </a>
+      </nav>
+
     </>
   )
 }
